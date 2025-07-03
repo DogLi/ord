05cd3fdc08c7d17d1622e39c431a90ec1c7d2db7
nandhh <nandhh@gmail.com>
2025-07-03 19:05:40 +0800

    feat: set single step active height

--- src/okx/brc20/policies.rs
+++ src/okx/brc20/policies.rs
@@ -17,8 +17,8 @@ impl HardForks {
 
   pub fn self_single_step_transfer_activation_height(chain: &Chain) -> u32 {
     match chain {
-      Chain::Mainnet => 900000,  // decided by community
-      Chain::Testnet => 2413343, //
+      Chain::Mainnet => 930000,  // decided by community
+      Chain::Testnet => 0, //
       Chain::Regtest => 0,
       Chain::Signet => 0,
     }
