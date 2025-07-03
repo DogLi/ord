2124105b940d9bb8a87ff70bf1796e73a5ab0f2a
nandhh <nandhh@gmail.com>
2025-07-03 17:06:08 +0800

    feat: support swap module deposit by single-step transfer

--- src/okx/brc20/executor/inscribe_transfer.rs
+++ src/okx/brc20/executor/inscribe_transfer.rs
@@ -1,8 +1,16 @@
+use std::ops::Add;
+
+use crate::okx::{
+  brc20::{brc20_decimal::Brc20Decimal, entry::BRC20ModuleTokenBalance},
+  utils::get_module_from_script,
+};
+
 use super::*;
 
 impl BRC20ExecutionMessage {
   pub(super) fn execute_inscribe_transfer(
     &self,
+    index: &Index,
     context: &mut TableContext,
   ) -> Result<BRC20Receipt, ExecutionError> {
     let BRC20Operation::InscribeTransfer{ signer, transfer } = &self.operation else {
@@ -17,16 +25,18 @@ impl BRC20ExecutionMessage {
     let ticker = BRC20Ticker::from_str(&transfer.tick).map_err(BRC20Error::TickerParse)?;
 
     // load ticker info, ensure the ticker is deployed
-    let ticker_info = context
+    let mut ticker_info = context
       .load_brc20_ticker_info(&ticker)?
       .ok_or(BRC20Error::TickerNotFound(transfer.tick.clone()))?;
 
-    let ticker = ticker_info.ticker;
+    let ticker = ticker_info.ticker.clone();
+    let decimals = ticker_info.clone().decimals;
+    let total_supply = ticker_info.clone().total_supply;
 
-    let amt = FixedPoint::new_from_str(&transfer.amount, ticker_info.decimals)
+    let amt = FixedPoint::new_from_str(&transfer.amount, decimals)
       .map_err(BRC20Error::NumericError)?;
     if amt.is_zero()
-      || amt > FixedPoint::new_unchecked(ticker_info.total_supply, ticker_info.decimals)
+      || amt > FixedPoint::new_unchecked(total_supply, decimals)
     {
       log::debug!(
         "brc20swap execute_inscribe_transfer amount invalid: {:?}, amount: {:?}",
@@ -58,7 +68,7 @@ impl BRC20ExecutionMessage {
       }
     }
 
-    let available = FixedPoint::new_unchecked(sender_balance.available, ticker_info.decimals);
+    let available = FixedPoint::new_unchecked(sender_balance.available, decimals);
     sender_balance.available = available
       .checked_sub(amt)
       .ok_or(ExecutionError::ExecutionFailed(
@@ -80,6 +90,48 @@ impl BRC20ExecutionMessage {
         .expect("Addition overflow");
 
       context.update_brc20_balance(&receiver, &ticker, receiver_balance)?;
+
+      // burn
+      let burned = receiver.op_return();
+      if burned {
+        ticker_info.burned = ticker_info
+          .burned
+          .checked_add(amount)
+          .expect("Addition overflow");
+
+        context.update_brc20_ticker_info(&ticker, ticker_info.clone())?;
+      }
+
+      // brc20 swap
+      let script_buf = receiver.to_script(&index.chain());
+      let module_id = get_module_from_script(&script_buf);
+      match module_id {
+        Ok(module_id_str) => {
+          log::info!(
+            "brc20swap transfer: {:?}, inscription_id: {:?}",
+            self.txid,
+            self.inscription_id
+          );
+          let is_deploy = context.is_module_already_deployed(&module_id_str)?;
+          if is_deploy {
+            let module_receiver = sender.clone();
+            let mut receiver_module_balance = context
+              .load_brc20_module_address_token_balance(&module_receiver, &module_id_str, &ticker)?
+              .unwrap_or(BRC20ModuleTokenBalance::new_with_scale(decimals));
+            receiver_module_balance.swap_account_balance = receiver_module_balance
+              .swap_account_balance
+              .add(Brc20Decimal::from_u128(amount, decimals).unwrap());
+            context.update_brc20_module_address_token_balance(
+              &module_receiver,
+              &module_id_str,
+              &ticker,
+              receiver_module_balance,
+            )?;
+          }
+        }
+        Err(_) => {}
+      }
+
     }
 
     context.update_brc20_balance(&sender, &ticker, sender_balance)?;
